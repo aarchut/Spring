e62274c46cb6b442c04b3ede97c14df9a827c562
Anna Archut <anna.archut@hcl.com>
2022-02-20 16:44:18 -0500

    Friday's exercise

---
+++ SpringBootRESTCRUD/src/main/java/com/hcl/model/Product.java
@@ -0,0 +1,29 @@
+package com.hcl.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+	@Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name="prd_id")
+	private Integer id; 
+	@Column(name = "prd_name")
+	private String name;
+	@Column(name = "prd_price")
+	private float price; 
+	
+
+}
---
+++ SpringBootRESTCRUD/src/main/java/com/hcl/service/ProductService.java
@@ -0,0 +1,38 @@
+package com.hcl.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hcl.model.Product;
+import com.hcl.repo.ProductRepository;
+
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository repo;
+
+	public List<Product> listAllProduct() {
+		return (List<Product>) repo.findAll();
+	}
+
+	public void createProduct(Product prd) {
+		repo.save(prd);
+	}
+
+	public Optional<Product> getProductsById(Integer id) {
+		return repo.findById(id);
+
+	}
+
+	public void deleteProduct(Integer id) {
+		repo.deleteById(id);
+	}
+	
+	public void updateProduct(Product prd) {
+		  repo.save(prd);
+	}
+
+}
